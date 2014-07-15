eb1eb67cc51a490400ff494de6e9674078910644
Linus Miller <lohfu@lohfu.io>
2014-07-15 15:21:07 +0200

    Added more directives for angular and started preparing for compatability with jqLite instead of jQuery.

--- js/spytext-angular.js
+++ js/spytext-angular.js
@@ -1,5 +1,41 @@
-var module = angular.module('spytext', []);
+var module = angular.module('Spytext', []);
 
-module.factory('SpyText', function($window) {
-	return $window.SpyText;
+module.factory('Spytext', function($window) {
+	return $window.Spytext;
+})
+
+// make two way binding work for contenteditable tags
+.directive('spytextField', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, element, attrs, ngModelCtrl) {
+            element.attr('contenteditable', 'true');
+            element.bind('keyup', function() {
+                if(!ngModelCtrl.$dirty && ngModelCtrl.$viewValue !== element.html()) {
+                    scope.$apply(function() {
+                        ngModelCtrl.$setViewValue(element.html());
+                    });
+                }
+            });
+            element.bind('blur', function() {
+                if(ngModelCtrl.$dirty) {
+                    scope.$apply(function() {
+                        ngModelCtrl.$setViewValue(element.html());
+                    });
+                }
+            });
+
+            ngModelCtrl.$render = function() {
+                element.html(ngModelCtrl.$viewValue);
+            };
+        }
+    };
+})
+.directive('spytextGroup', function(Spytext) {
+    return {
+        restrict: 'A',
+        link: function(scope,elem,attrs) {
+            Spytext.addGroup(elem);
+        }
+    };
 });
