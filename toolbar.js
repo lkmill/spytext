c09ac4a8d16efef24367b843d890d16c6d0e2f8b
Linus Miller <linus.miller@thecodebureau.com>
2015-08-21 16:16:19 +0200

    Moved command calling logic from toolbar view into field view. Toolbar simply calls the current fields command. This was to enable to the field to easily call commands via keybindings.

--- toolbar.js
+++ toolbar.js
@@ -1,6 +1,3 @@
-var commands = require('./commands');
-var selectron = require('./selectron');
-
 module.exports = {
 	events: {
 		'click button': 'command',
@@ -16,22 +13,11 @@ module.exports = {
 		this.field = field;
 		this.$el.toggleClass('active', !!field);
 	},
-
 	command: function(e) {
 		var command = $(e.currentTarget).attr('data-command'),
-			option = $(e.currentTarget).attr('data-option'),
-			field = this.field;
-
-		selectron.normalize();
-
-		field.snapback.register();
-
-		if(commands[command]) {
-			commands[command](field.el, option);
-
-			setTimeout(function() {
-				field.snapback.register();
-			}, 100);
-		}
+			option = $(e.currentTarget).attr('data-option');
+			
+		this.field.command(command, option);
 	}
+
 };
