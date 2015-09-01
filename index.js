6093ef52c05d80300a60c81c7a7249d022f017c3
Linus Miller <linus.miller@thecodebureau.com>
2015-09-01 22:32:54 +0200

    Comments.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * An HTML5 Rich Text Editor for the web designed to be used with Backbone
+ *
+ * @module spytext
+ */
+
 module.exports = {
 	views: {
 		SpytextToolbar: require('./toolbar'),
