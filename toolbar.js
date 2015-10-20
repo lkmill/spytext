f26b8721f0b5249fac977c1153e0c61aed8758f7
Linus Miller <lohfu@lohfu.io>
2015-10-20 10:40:15 +0200

    Added toolbar HTML (from template) to toolbar view, and small clean up in same view.

--- toolbar.js
+++ toolbar.js
@@ -4,12 +4,8 @@
  * @module spytext/toolbar 
  */
 
-function mapToNodeName(node) {
-	return node.nodeName;
-}
-
-var commands = require('./commands');
-var selectron = require('./selectron');
+var commands = require('./commands'),
+	selectron = require('./selectron');
 
 module.exports = require('ridge/view').extend({
 	/**
@@ -21,13 +17,15 @@ module.exports = require('ridge/view').extend({
 		'click button[data-command]': 'command',
 		'click button[data-undo]': 'undo',
 		'click button[data-redo]': 'redo',
-		mousedown: function(e) {
+		'mousedown .container': function(e) {
 			// this is needed to prevent toolbar from stealing focus
 			e.preventDefault();
 		}
 	},
 
-	template: 'spytext-toolbar',
+	render: function() {
+		return this.setElement($('<div class="spytext-toolbar"><div class="container"><ul class="spytext-button-group undo"><li><button class="spytext-button undo" data-undo tabindex="-1"></button></li><li><button class="spytext-button redo" data-redo tabindex="-1"></button></li></ul><ul class="spytext-dropdown block" data-command="block"><li><button data-option="h1">Heading 1</button></li><li><button data-option="h2">Heading 2</button></li><li><button data-option="h3">Heading 3</button></li><li><button data-option="h4">Heading 4</button></li><li><button data-option="h5">Heading 5</button></li><li><button data-option="h6">Heading 6</button></li><li><button data-option="p">Paragraph</button></li></ul><ul class="spytext-button-group format"><li><button class="spytext-button bold" data-command="format" data-option="strong" tabindex="-1"></button></li><li><button class="spytext-button italic" data-command="format" data-option="em" tabindex="-1"></button></li><li><button class="spytext-button underline" data-command="format" data-option="u" tabindex="-1"></button></li><li><button class="spytext-button strike-through" data-command="format" data-option="strike" tabindex="-1"></button></li><li><button class="spytext-button remove-format" data-command="removeFormat" tabindex="-1"></button></li></ul><ul class="spytext-button-group align"><li><button class="spytext-button align-left" data-command="align" data-option="left" tabindex="-1"></button></li><li><button class="spytext-button align-center" data-command="align" data-option="center" tabindex="-1"></button></li><li><button class="spytext-button align-right" data-command="align" data-option="right" tabindex="-1"></button></li><li><button class="spytext-button align-justify" data-command="align" data-option="justify" tabindex="-1"></button></li></ul><ul class="spytext-button-group list"><li><button class="spytext-button unordered-list" data-command="list" data-option="ul" tabindex="-1"></button></li><li><button class="spytext-button ordered-list" data-command="list" data-option="ol" tabindex="-1"></button></li></ul><ul class="spytext-button-group indent"><li><button class="spytext-button indent" data-command="indent" tabindex="-1"></button></li><li><button class="spytext-button outdent" data-command="indent" data-option="outdent" tabindex="-1"></button></li></ul></div></div>'));
+	},
 
 	/**
 	 * Activates or deactivates the toolbar depending on whether a spytext field
@@ -50,8 +48,6 @@ module.exports = require('ridge/view').extend({
 
 	setActiveStyles: function() {
 		if(!this.field) return this.toggle();
-
-		var _toolbar = this;
 
 		$('button[data-command]').each(function() {
 			var command = commands[$(this).attr('data-command')];
