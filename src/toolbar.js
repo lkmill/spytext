976062e417046818e84bc18fd12f4192e5c991c3
Linus Miller <lohfu@lohfu.io>
2016-05-22 18:41:18 +0200

    Made toolbar normal constructor, not a Backbone.View

--- src/toolbar.js
+++ src/toolbar.js
@@ -7,13 +7,28 @@
  */
 
 const commands = require('./commands'),
+  assign = require('object-assign'),
   selektr = require('selektr'),
+  dollr = require('dollr/dollr').$,
+  $$ = require('dollr/dollr').$$,
   ancestors = require('dollr/ancestors'),
   is = require('dollr/is'),
+  on = require('dollr/on'),
   uniq = require('lodash/uniq'),
+  forEach = require('lodash/forEach'),
   children = require('dollr/children');
 
-module.exports = require('ridge/view').extend({
+function Toolbar() {
+  this.el = dollr('<div class="spytext-toolbar"><div class="container"><ul class="spytext-button-group undo"><li><button class="spytext-button undo" data-undo tabindex="-1"></button></li><li><button class="spytext-button redo" data-redo tabindex="-1"></button></li></ul><ul class="spytext-dropdown block" data-command="block"><li><button data-option="h1">Heading 1</button></li><li><button data-option="h2">Heading 2</button></li><li><button data-option="h3">Heading 3</button></li><li><button data-option="h4">Heading 4</button></li><li><button data-option="h5">Heading 5</button></li><li><button data-option="h6">Heading 6</button></li><li><button data-option="p">Paragraph</button></li></ul><ul class="spytext-button-group format"><li><button class="spytext-button bold" data-command="format" data-option="strong" tabindex="-1"></button></li><li><button class="spytext-button italic" data-command="format" data-option="em" tabindex="-1"></button></li><li><button class="spytext-button underline" data-command="format" data-option="u" tabindex="-1"></button></li><li><button class="spytext-button strike-through" data-command="format" data-option="strike" tabindex="-1"></button></li><li><button class="spytext-button remove-format" data-command="removeFormat" tabindex="-1"></button></li></ul><ul class="spytext-button-group align"><li><button class="spytext-button link" data-command="link" tabindex="-1"></button></li></ul><ul class="spytext-button-group align"><li><button class="spytext-button align-left" data-command="align" data-option="left" tabindex="-1"></button></li><li><button class="spytext-button align-center" data-command="align" data-option="center" tabindex="-1"></button></li><li><button class="spytext-button align-right" data-command="align" data-option="right" tabindex="-1"></button></li><li><button class="spytext-button align-justify" data-command="align" data-option="justify" tabindex="-1"></button></li></ul><ul class="spytext-button-group list"><li><button class="spytext-button unordered-list" data-command="list" data-option="ul" tabindex="-1"></button></li><li><button class="spytext-button ordered-list" data-command="list" data-option="ol" tabindex="-1"></button></li></ul><ul class="spytext-button-group indent"><li><button class="spytext-button indent" data-command="indent" tabindex="-1"></button></li><li><button class="spytext-button outdent" data-command="indent" data-option="outdent" tabindex="-1"></button></li></ul></div></div>');
+
+  forEach(this.events, (fnc, eventStr) => {
+    const arr = eventStr.split(' ');
+
+    on($$(arr[1], this.el), arr[0], (fnc instanceof Function ? fnc : this[fnc]).bind(this));
+  });
+}
+
+assign(Toolbar.prototype, {
   /**
    * @lends SpytextToolbar.prototype
    * @augments Backbone.View
@@ -24,13 +39,10 @@ module.exports = require('ridge/view').extend({
     'click button[data-undo]': 'undo',
     'click button[data-redo]': 'redo',
     'mousedown .container': function (e) {
+      console.log('preventing mousedown');
       // this is needed to prevent toolbar from stealing focus
       e.preventDefault();
     }
-  },
-
-  render() {
-    return this.setElement($('<div class="spytext-toolbar"><div class="container"><ul class="spytext-button-group undo"><li><button class="spytext-button undo" data-undo tabindex="-1"></button></li><li><button class="spytext-button redo" data-redo tabindex="-1"></button></li></ul><ul class="spytext-dropdown block" data-command="block"><li><button data-option="h1">Heading 1</button></li><li><button data-option="h2">Heading 2</button></li><li><button data-option="h3">Heading 3</button></li><li><button data-option="h4">Heading 4</button></li><li><button data-option="h5">Heading 5</button></li><li><button data-option="h6">Heading 6</button></li><li><button data-option="p">Paragraph</button></li></ul><ul class="spytext-button-group format"><li><button class="spytext-button bold" data-command="format" data-option="strong" tabindex="-1"></button></li><li><button class="spytext-button italic" data-command="format" data-option="em" tabindex="-1"></button></li><li><button class="spytext-button underline" data-command="format" data-option="u" tabindex="-1"></button></li><li><button class="spytext-button strike-through" data-command="format" data-option="strike" tabindex="-1"></button></li><li><button class="spytext-button remove-format" data-command="removeFormat" tabindex="-1"></button></li></ul><ul class="spytext-button-group align"><li><button class="spytext-button link" data-command="link" tabindex="-1"></button></li></ul><ul class="spytext-button-group align"><li><button class="spytext-button align-left" data-command="align" data-option="left" tabindex="-1"></button></li><li><button class="spytext-button align-center" data-command="align" data-option="center" tabindex="-1"></button></li><li><button class="spytext-button align-right" data-command="align" data-option="right" tabindex="-1"></button></li><li><button class="spytext-button align-justify" data-command="align" data-option="justify" tabindex="-1"></button></li></ul><ul class="spytext-button-group list"><li><button class="spytext-button unordered-list" data-command="list" data-option="ul" tabindex="-1"></button></li><li><button class="spytext-button ordered-list" data-command="list" data-option="ol" tabindex="-1"></button></li></ul><ul class="spytext-button-group indent"><li><button class="spytext-button indent" data-command="indent" tabindex="-1"></button></li><li><button class="spytext-button outdent" data-command="indent" data-option="outdent" tabindex="-1"></button></li></ul></div></div>'));
   },
 
   /**
@@ -39,7 +51,7 @@ module.exports = require('ridge/view').extend({
    */
   toggle(field) {
     this.field = field;
-    this.$el.toggleClass('active', !!field);
+    this.el.classList.toggle('active', !!field);
   },
 
   undo() {
@@ -129,8 +141,8 @@ module.exports = require('ridge/view').extend({
     });
 
     // use undoIndex in snapback instance to decide whether we can undo/redo
-    this.$('button[data-undo]').prop('disabled', this.field.snapback.undoIndex === -1);
-    this.$('button[data-redo]').prop('disabled', this.field.snapback.undoIndex >= (this.field.snapback.undos.length - 1));
+    $('button[data-undo]', this.el).prop('disabled', this.field.snapback.undoIndex === -1);
+    $('button[data-redo]', this.el).prop('disabled', this.field.snapback.undoIndex >= (this.field.snapback.undos.length - 1));
   },
 
   listCommand(e) {
@@ -149,3 +161,5 @@ module.exports = require('ridge/view').extend({
     this.field.command(command, option);
   }
 });
+
+module.exports = Toolbar;
